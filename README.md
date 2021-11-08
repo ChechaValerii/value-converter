b8336c36ca4f2547454358bf1dc11517280d3699
alenworld <inalipa.alen@gmail.com>
2021-11-08 16:46:53 +0200

    added new funcs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Converter
+# Install
+```
+# with npm
+npm install 
+```
+
+# Usage
+```javascript
+const converter = require('converter')
+
+console.log(converter.celsiumToFahrenheit(5)) // return 41°F
+```
--- index.js
+++ index.js
@@ -8,7 +8,31 @@ function fahrenheitToCelsium(number) {
   return Cels;
 }
 
+function metersToFeet(number) {
+  const Feet = `${number * 3.281} ft`;
+  return Feet;
+}
+
+function feetToMeters(number) {
+  const Metre = `${number / 3.281} mt`;
+  return Metre;
+}
+
+function lightsecondToKm(number) {
+  const Km = `${number * 299792} km`;
+  return Km;
+}
+
+function kmToLightsecond(number) {
+  const Ls = `${(number / 299792).toFixed(7)} light second(s)`;
+  return Ls;
+}
+
 module.exports = {
   celsiumToFahrenheit,
   fahrenheitToCelsium,
+  metersToFeet,
+  feetToMeters,
+  lightsecondToKm,
+  kmToLightsecond,
 };
